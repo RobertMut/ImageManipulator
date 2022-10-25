f3ecc23eb4da04f2be1d5b821391770a40ba69d9
Robert Mutniański <robert.mutnianski@inetum.com>
2022-10-25 22:10:52 +0200

    Add window

---
+++ Core/Application/ViewModels/ContrastStretchingWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ReactiveUI;
+
+namespace ImageManipulator.Application.ViewModels
+{
+	public class ContrastStretchingWindowViewModel : ReactiveObject
+	{
+		public ContrastStretchingWindowViewModel(I)
+	}
+}
---
+++ Presentation/Presentation/Views/ContrastStretchingWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ImageManipulator.Presentation.Views
+{
+    public partial class ContrastStretchingWindow : Window
+    {
+        public ContrastStretchingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
