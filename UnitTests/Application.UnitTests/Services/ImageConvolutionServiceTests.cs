1e05f09fcec5abf471a276540587917c38a55cb8
Robert Mutniański <robert.mutnianski@inetum.com>
2023-10-29 22:45:43 +0100

    unit tests

---
+++ UnitTests/Application.UnitTests/Services/ImageConvolutionServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Text.Json;
+using Core;
+using ImageManipulator.Application.Common.Interfaces;
+using ImageManipulator.Application.Common.Services;
+using NUnit.Framework;
+
+namespace Application.UnitTests.Services;
+
+[ExcludeFromCodeCoverage]
+[TestFixture]
+public class ImageConvolutionServiceTests
+{
+    private IImageConvolutionService _imageConvolutionService;
+    private Bitmap _testImage;
+
+    private readonly double[,] _kernel = {
+        { 0, -1, 0 },
+        { -1, 4, -1 },
+        { 0, -1, 0 }
+    };
+    
+    [SetUp]
+    public async Task SetUp()
+    {
+        _testImage = new Bitmap("Resources/image.png");
+        _imageConvolutionService = new ImageConvolutionService();
+    }
+
+    [Test]
+    public async Task ImagePointOperationsServiceComputesGradient()
+    {
+        double[,] expected = GetGradientFromFile(await File.ReadAllTextAsync("Resources/gradient"));
+        double[,] gradient = _imageConvolutionService.ComputeGradient(_testImage, (gx, gy) => Math.Sqrt(gx * gx + gy * gy));
+        
+        Assert.That(gradient, Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesHysteresisThresholdWithSpecifiedGradient()
+    {
+        double[,] gradient = GetGradientFromFile(await File.ReadAllTextAsync("Resources/gradient"));
+        Bitmap expectedImage = new Bitmap("Resources/image_hysteresis.png");
+        Bitmap bitmap = _imageConvolutionService.HysteresisThresholding(512, 512, 10, 255, gradient);
+        
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesWithSpecifiedKernelAndSharpen()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_convolution_sharpen.png");
+        Bitmap bitmap = _imageConvolutionService.Execute(_testImage, _kernel);
+        
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesWithSpecifiedKernelAndSoften()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_convolution_soften.png");
+        Bitmap bitmap = _imageConvolutionService.Execute(_testImage, _kernel, true);
+        
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    private static double[,] GetGradientFromFile(string fileWithArray)
+    {
+        string[] lines = fileWithArray.Split("\r\n");
+        
+        double[,] array = new double[lines.Length, lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] line = lines[i].Split("\t");
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                if (!string.IsNullOrEmpty(line[j]))
+                {
+                    array[i, j] = double.Parse(line[j]);
+                }
+            }
+        }
+
+        return array;
+    }
+}
--- UnitTests/Application.UnitTests/Services/ImagePointOperationsServiceTests.cs
+++ UnitTests/Application.UnitTests/Services/ImagePointOperationsServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Text.Json;
 using Core;
 using ImageManipulator.Application.Common.Interfaces;
 using ImageManipulator.Application.Common.Services;
@@ -39,5 +40,159 @@ public class ImagePointOperationsServiceTests
         byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
         
         Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesThresholdAndDontReplacesColors()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_threshold.png");
+        Bitmap bitmap = _imagePointOperationsService.Thresholding(_testImage, 128, false);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesHistogramEqualization()
+    {
+        int[][]? lut = JsonSerializer.Deserialize<int[][]>(await File.ReadAllTextAsync("Resources/ImageLUT.json"));
+        Bitmap expectedImage = new Bitmap("Resources/image_histogram_eq.png");
+
+        Bitmap bitmap = _imagePointOperationsService.HistogramEqualization(_testImage, lut);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesStretchContrast()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_stretch_contrast.png");
+
+        Bitmap bitmap = _imagePointOperationsService.StretchContrast(_testImage, 57, 170);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesMultiThresholdWithReplace()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_multithreshold_replace.png");
+
+        Bitmap bitmap = _imagePointOperationsService.MultiThresholding(_testImage, 75, 170);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesMultiThresholdWithoutReplace()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_multithreshold.png");
+
+        Bitmap bitmap = _imagePointOperationsService.MultiThresholding(_testImage, 75, 170, false);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesNonLinearlyStretchContrastWithoutReplace()
+    {
+        Bitmap expectedImage = new Bitmap("Resources/image_nonlinstretch.png");
+
+        Bitmap bitmap = _imagePointOperationsService.NonLinearlyStretchContrast(_testImage, 1.5d);
+
+        Bitmap pngBitmap;
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            pngBitmap = new Bitmap(stream);
+        }
+        
+        byte[] expectedBytes = ImageHelper.ImageToByte(expectedImage);
+        byte[] actualBytes = ImageHelper.ImageToByte(pngBitmap);
+        
+        Assert.That(actualBytes, Is.EqualTo(expectedBytes));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesCalculateLowerImageThreshold()
+    {
+        int[]? histogram = JsonSerializer.Deserialize<int[]>(await File.ReadAllTextAsync("Resources/ImageLuminance.json"));
+
+        int lowerImageThresholdPoint = _imagePointOperationsService.CalculateLowerImageThresholdPoint(histogram);
+        
+        Assert.That(lowerImageThresholdPoint, Is.EqualTo(99));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesCalculateUpperImageThreshold()
+    {
+        int[]? histogram = JsonSerializer.Deserialize<int[]>(await File.ReadAllTextAsync("Resources/ImageLuminance.json"));
+
+        int upperImageThresholdPoint = _imagePointOperationsService.CalculateUpperImageThresholdPoint(histogram);
+        
+        Assert.That(upperImageThresholdPoint, Is.EqualTo(255));
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesCalculateLowerImageThresholdAndThrowsException()
+    {
+        Assert.Throws<NullReferenceException>(
+            () => _imagePointOperationsService.CalculateLowerImageThresholdPoint(null), "Histogram is null");
+    }
+    
+    [Test]
+    public async Task ImagePointOperationsServiceExecutesCalculateUpperImageThresholdAndThrowsException()
+    {
+        Assert.Throws<NullReferenceException>(
+            () => _imagePointOperationsService.CalculateUpperImageThresholdPoint(null), "Histogram is null");
     }
 }
